bc7cf30f55cd76d265255aa3103f7768f2f6bf0f
Pelliccia96 <fra.pelliccia96@gmail.com>
2023-02-21 13:23:33 +0100

    Fixed filePath attachment in ContactController

--- app/Http/Controllers/Api/ContactController.php
+++ app/Http/Controllers/Api/ContactController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Models\Contact;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class ContactController extends Controller
 {
@@ -15,6 +16,15 @@ class ContactController extends Controller
             "message" => "required|string",
             "attachment" => "nullable|file|max:5000",
         ]);
+
+        // se ricevo un allegato, ne salvo il file ed il percorso
+        if ($request->has("attachment")) {
+            // recupero il path del file appena caricato
+            $filePath = Storage::put("/contacts", $data["attachment"]);
+
+            //lo assegno alla chiave attachment
+            $data["attachment"] = $filePath;
+        }
 
         // salviamo i dati ricevuti dentro una tabella dedicata
         $newContact = Contact::create($data);
